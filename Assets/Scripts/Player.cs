14f7c8db65fd4827f572115d3477ec43859361d4
13joaquin <juaquin.david13@gmail.com>
2023-07-12 17:48:10 -0600

    Clear Counter

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,9 +6,36 @@ public class Player : MonoBehaviour
 { 
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask countersLayerMask;
     private bool isWalking;
+    private Vector3 lastInteractDir;
     private void Update(){
+       HandheldMovement();
+       HandheldInteractions();
+    }
+    public bool IsWalking(){
+        return isWalking;
+    }
+    private void HandheldInteractions(){
        Vector2 inputVector = gameInput.GetMovementVectorNormlized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+        }
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {
+            if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)){
+                //Tiene ClearCounter
+                clearCounter.Interact();
+            }
+        }
+    }
+    private void HandheldMovement(){
+        Vector2 inputVector = gameInput.GetMovementVectorNormlized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         
@@ -52,9 +79,6 @@ public class Player : MonoBehaviour
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward,moveDir, Time.deltaTime * rotateSpeed);
         //Debug.Log(Time.deltaTime);
-    }
-    public bool IsWalking(){
-        return isWalking;
     }
     /* Start is called before the first frame update
     void Start()
