b21973267d0b29a9b66d22bd53b656353fe889d5
13joaquin <juaquin.david13@gmail.com>
2023-08-04 20:48:41 -0600

    Update 4 August
    Plate World Complete Icons And Delivery Counter Part One

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -1,9 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject
 {
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs
+    {
+        public KitchenObjectSO KitchenObjectSO;
+    }
     [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
     private List<KitchenObjectSO> kitchenObjectSOList;
     private void Awake() {
@@ -19,7 +25,13 @@ public class PlateKitchenObject : KitchenObject
             return false;
         }else{
             kitchenObjectSOList.Add(kitchenObjectSO);
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs{
+                KitchenObjectSO = kitchenObjectSO
+            });
             return true;
         }
+    }
+    public List<KitchenObjectSO>GetKitchenObjectSOList(){
+        return kitchenObjectSOList;
     }
 }
