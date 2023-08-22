a653dc90ea6f2eb7b3faa83a8f23565537b809f9
13joaquin <juaquin.david13@gmail.com>
2023-08-22 17:48:24 -0600

    Update 22 August
    Second Part fully complete of Game Over

--- Assets/Scripts/KichenGameManager.cs
+++ Assets/Scripts/KichenGameManager.cs
@@ -16,7 +16,8 @@ public class KichenGameManager : MonoBehaviour
     private State state;
     private float WaitingToStartTimer = 1f;
     private float countdownToStartTimer = 3f;
-    private float gamePlayingTimer = 10f;
+    private float gamePlayingTimer;
+    private float gamePlayingTimerMax = 20f;
     private void Awake() {
         Instance = this;
         state = State.WaitingToStart;
@@ -34,6 +35,7 @@ public class KichenGameManager : MonoBehaviour
                 countdownToStartTimer -= Time.deltaTime;
                 if(countdownToStartTimer < 0f){
                     state = State.GamePlaying;
+                    gamePlayingTimer = gamePlayingTimerMax;
                     OnStateChanged?.Invoke(this, EventArgs.Empty);
                 }
                 break;
@@ -60,5 +62,8 @@ public class KichenGameManager : MonoBehaviour
     }
     public bool IsGameOver(){
         return state == State.GameOver;
+    }
+    public float GetGamePlayingTimerNormalized(){
+        return 1 - (gamePlayingTimer / gamePlayingTimerMax);
     }
 }
